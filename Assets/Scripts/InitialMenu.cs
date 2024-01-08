bb7f2b6556b98193e3e001941ff9004c68e437cc
polarsama <andreska9506@gmail.com>
2024-01-07 21:09:04 -0500

    Create levels scene and buttons 1234 lvl

--- Assets/Scripts/InitialMenu.cs
+++ Assets/Scripts/InitialMenu.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InitialMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //llamar escena para jugar
+   public void Jugar(){
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Salir(){
+        Debug.Log("Salir...");
+        Application.Quit();
     }
 }
